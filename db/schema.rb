d915ea9ef541a3a921ef740c1cdb7c7be24ffb18
MichelleMHey <michellemhey@gmail.com>
2013-06-06 10:01:32 -0500

    changed name of migration table to 'tasks'

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(:version => 20130605114534) do
 
-  create_table "posts", :force => true do |t|
+  create_table "tasks", :force => true do |t|
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
