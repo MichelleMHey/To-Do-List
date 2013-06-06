25624d55ed499925c743e7e72f6f5e0e9944863f
MichelleMHey <michellemhey@gmail.com>
2013-06-06 10:03:59 -0500

    added database cleaner

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,33 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
+require 'capybara/rails'
+#Capybara.default_driver = :selenium
 
+DatabaseCleaner.strategy = :transaction
+
+class ActiveSupport::TestCase
+  def setup
+    DatabaseCleaner.start
+  end
+  def teardown
+    DatabaseCleaner.clean
+  end
+end
+
+class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  self.use_transactional_fixtures = false
+end
+
+
+# module DatabaseCleaner
+# 	def before_setup
+# 	  super
+# 	  Task.destroy_all
+# 	end
+# end
